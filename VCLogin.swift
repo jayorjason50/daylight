06710a907015e64703584f14f24b845e7d7471e3
jayorjason50 <jasonspence@Jasons-Mac-mini.home>
2015-07-21 16:58:58 +0100

    facebook dude

--- VCLogin.swift
+++ VCLogin.swift
@@ -10,10 +10,52 @@ import UIKit
 
 class VCLogin: UIViewController {
 
+    @IBOutlet weak var login_FB: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        
+        login_FB.addTarget(self, action: "loginFB:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+          // Do any additional setup after loading the view.
+    }
+    
+    func loginFB(sender:UIButton){
+    
+        let ref = Firebase(url: "https://miquiz.firebaseio.com")
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"], handler: {
+            (facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                println("Facebook login failed. Error \(facebookError)")
+            } else if facebookResult.isCancelled {
+                println("Facebook login was cancelled.")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                ref.authWithOAuthProvider("facebook", token: accessToken,
+                    withCompletionBlock: { error, authData in
+                        
+                        if error != nil
+                        {
+                           println("Login failed. \(error)")
+                        }
+                        else
+                        {
+                            
+                            println("TEST")
+                            
+                            var view: VCMyQuizzes = self.storyboard?.instantiateViewControllerWithIdentifier("VCMyQuizzes") as! VCMyQuizzes
+                            self.navigationController?.pushViewController(view, animated: true)
+                            view.title = "miQuizzes" //Set the text
+                            println("Logged in! \(authData)")
+                        }
+                })
+            }
+        })
+        println("Function Works")
+    
     }
 
     override func didReceiveMemoryWarning() {
